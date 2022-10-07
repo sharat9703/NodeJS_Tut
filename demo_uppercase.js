65cd542edec9a2c9a6de670bb7b5a2dbaa2733d3
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 <sharath.yaragatti@yahoo.com>
2022-10-07 15:03:25 +0530

    demo_uppercase.js

--- demo_uppercase.js
+++ demo_uppercase.js
@@ -2,6 +2,6 @@ let http = require('http');
 let uc =require('upper-case');
 http.createServer((req,res)=>{
     res.writeHead(200,{"Content-Type":"text/html"});
-    res.write(uc.upperCase('Hello world!'));
+    res.write(uc.upperCase('hello world!'));
     res.end();
 }).listen(8080);
