19e0d510cb1ca3143e675db05aca96534170feb0
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 <sharath.yaragatti@yahoo.com>
2022-10-07 11:17:40 +0530

    url module

---
+++ demo_url_module.js
@@ -0,0 +1,15 @@
+let url = require('url');
+
+let adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+ 
+let q = url.parse(adr,true);
+
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
+
+let qdata = q.query;
+
+console.log(qdata.year);
+
+console.log(qdata.month);
